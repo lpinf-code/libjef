1eb68bd7fae48b819f639eb62005475ad2f0788d
Perle Pigache <leonard.pigache@hotmail.com>
2024-05-23 14:38:16 +0200

    feat: started implementation of JSON parsing

---
+++ src/parser/serialize.c
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2024
+** jef
+** File description:
+** src/parser/serialize.c
+*/
+
+#include "jef/json.h"
+
+/**
+ * @brief Turns an entity into a valid JSON string.
+ *
+ * @param entity The entity to serialize.
+ * @param outptr The pointer to store the string to.
+ * @param indent Indentation for objects/arrays, -1 to disable formatting.
+ * @returns On success, returns the string buffer size. -1 on failure.
+ */
+ssize_t json_serialize(json_entity_t *entity, char **outptr, int indent)
+{
+    return -1;
+}
---
+++ src/parser/unserialize.c
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2024
+** jef
+** File description:
+** src/parser/unserialize.c
+*/
+
+#include "jef/json.h"
+
+/**
+ * @brief Turns a JSON string into an entity.
+ *
+ * @param input The string to unserialize.
+ * @returns An entity containing the data stored in the string. NULL on error.
+ */
+json_entity_t *json_unserialize(const char *input)
+{
+    return NULL;
+}
